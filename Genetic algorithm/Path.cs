e8c2d16fc0df0a9b9b2917103370f66c51472bc6
Vostok17 <gagarin.artem.2008@gmail.com>
2021-11-12 01:06:11 +0300

    Add path class with chromosome and fitness function

---
+++ Genetic algorithm/Path.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetic_algorithm
+{
+    internal class Path
+    {
+        public int[] Chromosome;
+        public int Fitness;
+        public Path(int[] chromosome, Graph distance)
+        {
+            Chromosome = chromosome;
+            Fitness = FitnessFunction(distance);
+        }
+        public int FitnessFunction(Graph distance)
+        {
+            int valueOfPath = 0;
+            for (int i = 1; i < distance.Size; i++)
+            {
+                int curr = Chromosome[i - 1];
+                int next = Chromosome[i];
+                valueOfPath += distance[curr, next];
+            }
+            // Return to start
+            int last = Chromosome[distance.Size - 1];
+            int start = Chromosome[0];
+            valueOfPath += distance[last, start];
+
+            return valueOfPath;
+        }
+    }
+}
